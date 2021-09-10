272eeb1cee7360266d76cd518ee9b872c9329311
oona-k <59806531+oona-k@users.noreply.github.com>
2021-09-10 17:36:21 +0300

    Build files

---
+++ build/precache-manifest.4f529c57deaaeed2db621f25f984bba3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce10bbefc78249195780a4f6e5b4a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa051065267475b01a96",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9d14af186367686de0f8",
+    "url": "/static/css/main.5fc2c5d0.chunk.css"
+  },
+  {
+    "revision": "fa051065267475b01a96",
+    "url": "/static/js/2.2e7c4bd5.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/static/js/2.2e7c4bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d14af186367686de0f8",
+    "url": "/static/js/main.200a4d91.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "0e8b6873e9e905c624a888f3ba73aa42",
+    "url": "/static/media/mtg-rules.0e8b6873.PNG"
+  },
+  {
+    "revision": "e498208e95bb1bcacec64673f371739a",
+    "url": "/static/media/music-app.e498208e.PNG"
+  },
+  {
+    "revision": "39772faae4f16ea02a5c37231ec7ca42",
+    "url": "/static/media/myphoto.39772faa.jpg"
+  }
+]);
